8f0050eb46058378c6dd5775a75238373d00c770
David Björklund <david.bjorklund@gmail.com>
2014-08-12 19:07:21 -0400

    new option: escape newlines

--- csv-line.js
+++ csv-line.js
@@ -1,7 +1,13 @@
 module.exports = function (options) {
   var separator = options && options.separator ? options.separator : ','
+    , escapeNewlines = options && options.escapeNewlines === true
     , regexp = new RegExp('[' + separator + '\r\n"]')
     , escape = function (cell) {
+
+        if (escapeNewlines) {
+          cell = cell.replace(/\n/g, '\\n')
+        }
+
         return regexp.test(cell) ? '"' + cell.replace(/"/g, '""') + '"' : cell
       }
 
--- example.js
+++ example.js
@@ -1,6 +1,10 @@
 var csvLine = require('./csv-line')
   , toCsv1 = csvLine()
   , toCsv2 = csvLine({ separator: '*' })
+  , toCsv3 = csvLine({ escapeNewlines: true })
 
-console.log(toCsv1([ '"hello"', 'I am a *', 'Girfriend in a ,' ]))
-console.log(toCsv2([ 'I am a *', 'Girfriend in a ,' ]))
+console.log(toCsv1([ '"hello"', 'I am a *', 'Girfriend in a ,', 'new\nline' ]))
+console.log()
+console.log(toCsv2([ 'I am a *', 'Girlfriend in a ,', 'new\nline' ]))
+console.log()
+console.log(toCsv3([ 'I am a *', 'Girlfriend in a ,', 'new\nline']))
